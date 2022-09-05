5c858c0c2ebd09cdca30c8740be8f5c526b2cb7a
jlee7x <98127351+jlee7x@users.noreply.github.com>
2022-09-05 15:14:47 -0700

    Update Final_Project_App.py

--- Final_Project_App.py
+++ Final_Project_App.py
@@ -32,9 +32,9 @@ The app gives each user following parameters to choose:
 '''
 )
 
-col_names = ["none", "year", "month", "state", "city_or_county", "n_killed", "n_injured", "latitude", "longitude"]
-df = pd.read_csv('https://github.com/jlee7x/DE_App_Project/blob/main/Final_Project_Data.csv',names=col_names, sep="delimiter")
-df = df.drop(columns=['none','month'])
+#col_names = ["none", "year", "month", "state", "city_or_county", "n_killed", "n_injured", "latitude", "longitude"]
+df = pd.read_csv('https://github.com/jlee7x/DE_App_Project/blob/main/Final_Project_Data.csv', sep=",", header=None)
+#df = df.drop(columns=['none','month'])
 st.dataframe(df)
 
 # Parameter Selection Section
