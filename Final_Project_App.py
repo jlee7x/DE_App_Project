0f32c449fcd42ee9a5b6eb039b8c37613b7c011a
jlee7x <98127351+jlee7x@users.noreply.github.com>
2022-09-05 14:33:21 -0700

    Update Final_Project_App.py

--- Final_Project_App.py
+++ Final_Project_App.py
@@ -33,7 +33,7 @@ The app gives each user following parameters to choose:
 )
 
 df = pd.read_csv('https://github.com/jlee7x/DE_App_Project/blob/main/Final_Project_Data.csv')
-df = df.drop(columns=['Unnamed: 0','month'])
+df = df.drop(columns=['month'])
 
 # Parameter Selection Section
 st.write("## Parameter Selector")
