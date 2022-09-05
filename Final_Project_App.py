7e1f6db998251e0aed25ad68ecc6fe17a4d5199e
jlee7x <98127351+jlee7x@users.noreply.github.com>
2022-09-05 15:19:31 -0700

    Update Final_Project_App.py

--- Final_Project_App.py
+++ Final_Project_App.py
@@ -32,8 +32,8 @@ The app gives each user following parameters to choose:
 '''
 )
 
-col_names = ["none", "year", "month", "state", "city_or_county", "n_killed", "n_injured", "latitude", "longitude"]
-df = pd.read_csv('https://github.com/jlee7x/DE_App_Project/blob/main/Final_Project_Data.csv', names=col_names, error_bad_lines=False)
+#col_names = ["none", "year", "month", "state", "city_or_county", "n_killed", "n_injured", "latitude", "longitude"]
+df = pd.read_csv('https://github.com/jlee7x/DE_App_Project/blob/main/Final_Project_Data.csv', error_bad_lines=False)
 #df = df.drop(columns=['none','month'])
 st.dataframe(df)
 
