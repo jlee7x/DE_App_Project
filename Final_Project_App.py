5394700142d8b591cbf0f6171aba488bd69480ac
jlee7x <98127351+jlee7x@users.noreply.github.com>
2022-09-05 14:53:10 -0700

    Update Final_Project_App.py

--- Final_Project_App.py
+++ Final_Project_App.py
@@ -32,8 +32,8 @@ The app gives each user following parameters to choose:
 '''
 )
 
-df = pd.read_csv('https://github.com/jlee7x/DE_App_Project/blob/main/Final_Project_Data.csv')
-df = df.drop(columns=['month'])
+df = pd.read_csv('https://github.com/jlee7x/DE_App_Project/blob/main/Final_Project_Data.csv',sep='delimiter')
+df = df.drop(columns=[' ','month'])
 
 # Parameter Selection Section
 st.write("## Parameter Selector")
